d0e16f7e0e1e40a61deb199c6f29c78da11481dc
Jesse Robinson <j3robinson1@gmail.com>
2015-03-01 19:58:36 -0500

    still dont get this img attachment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ end
 
 def create
    @user = current_user
-   @post = @user.create_post(post_params)
+   @post = @user.posts.create(post_params)
    respond_to do |format|
      if @post.save
        params[:post_attachments]['avatar'].each do |a|
@@ -66,11 +66,11 @@ end
     end
   end
 
-  private
+private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
    def post_params
